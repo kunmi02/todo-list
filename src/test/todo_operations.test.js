bbbe1d8e76d4f80d65089e0bb6fd903bf20cf9e5
kunmi02 <mrkhunmie@gmail.com>
2021-04-18 21:16:38 +0100

    fix linter errors

--- src/test/todo_operations.test.js
+++ src/test/todo_operations.test.js
@@ -5,7 +5,7 @@ import Task from '../modules/todo';
 test('should create a new task object ', () => {
   const project = new Project('Test Project', 'My description');
   const Newtask = new Task('Test Task', 'My description', '1/2/3', 'meduim');
-  const tasks = project.projectTask
-  const savedTask = tasks.push(Newtask)
+  const tasks = project.projectTask;
+  const savedTask = tasks.push(Newtask);
   expect(savedTask).toBe(1);
 });
